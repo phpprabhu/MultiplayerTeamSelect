51f7f6ccd1a842b1d3a349675cf73686139c31cf
Prabhu <p.santana@global.leaseweb.com>
2022-05-23 18:12:00 +0530

    Server

---
+++ server/server.js
@@ -0,0 +1,69 @@
+// npm install express,  socket.io
+// npx nodemon server.js
+// npx live-server
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+var game_start = false;
+var blasted_balls = []
+const no_of_balls = 500;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://127.0.0.1:8080",
+    methods: ["GET", "POST"],
+    transports: ['websocket', 'polling'],
+    credentials: true
+  },
+  allowEIO3: true
+ });
+
+io.on('connection', (sock) => {
+    console.log("Someone connected");
+    sock.emit('init', blasted_balls);
+
+    sock.on('blast_state', (blasted_ball) => {
+        if (game_start) {
+            if (!blasted_balls.includes(blasted_ball)) {
+                blasted_balls.push(blasted_ball)
+            }
+            io.emit('current_state', blasted_balls)
+
+            if (blasted_balls.length == no_of_balls - 1) {
+                io.emit('gameOver', 'winner selected');
+            }
+        }
+    });
+
+    sock.on('clear_game', (clear) => {
+        blasted_balls = []
+        game_start = false;
+        io.emit('current_state', blasted_balls)
+    });
+
+    sock.on('start_game', (start) => {
+        blasted_balls = []
+        io.emit('countdown', 'show')
+        io.emit('current_state', blasted_balls)
+        setTimeout(function () {
+            game_start = true;
+        }, 3500);
+    });
+});
+
+server.on('error', (err) => {
+    console.log(err);
+});
+
+function emitGameState(blasted_balls) {
+  // Send this event to everyone
+    sock.emit('gameState', blasted_balls);
+}
+
+
+server.listen(8000, ()=>{
+console.log('Started on 8000')});
